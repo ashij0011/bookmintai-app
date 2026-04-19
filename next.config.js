7e7590f133058615e0a27a346bb74f35aede0328
Ashish Jha <ashij0011@gmail.com>
2026-04-18 23:41:48 -0400

    Fix module exports and import paths

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
 };
 
 module.exports = nextConfig;
